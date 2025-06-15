cbe1f5eedb36deae947e0aea68d588acdd6baf4f
Fabricio Pagliarini <fabriciopgl87@gmail.com>
2025-06-15 16:16:55 -0300

    Add CORS policy and HTTPS for production

--- src/ToDoList.WebApi/Startup.cs
+++ src/ToDoList.WebApi/Startup.cs
@@ -1,13 +1,13 @@
-using AspNetCoreRateLimit;
 using TodoList.WebApi.DependencyInjection;
 using TodoList.WebApi.Middlewares;
 using ToDoList.WebApi.DependencyInjection;
 
 namespace ToDoList.WebApi;
 
-public class Startup(IConfiguration configuration)
+public class Startup(IConfiguration configuration, IWebHostEnvironment environment)
 {
     public IConfiguration Configuration { get; } = configuration;
+    public IWebHostEnvironment Environment { get; } = environment;
 
     public void ConfigureServices(IServiceCollection services)
     {
@@ -21,21 +21,38 @@ public class Startup(IConfiguration configuration)
         services.AddDatabase(Configuration);
         services.AddGlobalExceptionHandler();
         services.AddRateLimiting(Configuration);
+        services.AddHttpsConfiguration(Environment);
         services.AddJwtAuthentication(Configuration);
+        services.AddCorsConfiguration(Configuration);
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
+            app.UseCors("DevelopmentPolicy");
             app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseCors("ProductionPolicy");
+        }
+
+        app.Use((context, next) =>
+        {
+            context.Response.Headers.XContentTypeOptions = "nosniff";
+            context.Response.Headers.XFrameOptions = "DENY";
+            context.Response.Headers.XXSSProtection = "1; mode=block";
+
+            return next();
+        });
 
         app.UseRouting();
 
         app.UseMiddleware<JsonRateLimitMiddleware>();
 
-        app.UseIpRateLimiting();
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseExceptionHandler();
---
+++ src/TodoList.WebApi/DependencyInjection/CorsInjection.cs
@@ -0,0 +1,32 @@
+﻿namespace TodoList.WebApi.DependencyInjection;
+
+public static class CorsInjection
+{
+    public static void AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("DevelopmentPolicy", builder =>
+            {
+                builder.WithOrigins(
+                        "http://localhost:3000",      // React
+                        "http://localhost:4200",      // Angular  
+                        "http://localhost:5173"       // Vite
+                    )
+                    .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                    .WithHeaders("Content-Type", "Authorization")
+                    .AllowCredentials();
+            });
+
+            var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>() ?? ["https://yourdomain.com"];
+
+            options.AddPolicy("ProductionPolicy", builder =>
+            {
+                builder.WithOrigins(allowedOrigins)
+                       .WithMethods("GET", "POST", "PATCH", "DELETE")
+                       .WithHeaders("Content-Type", "Authorization")
+                       .AllowCredentials();
+            });
+        });
+    }
+}
